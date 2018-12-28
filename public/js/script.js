f364e085806190c95edb25eb2a91680dd670f09e
hanherb <dyo.pyon@gmail.com>
2018-12-28 18:58:51 +0700

    small fix

--- public/js/script.js
+++ public/js/script.js
@@ -132,11 +132,10 @@ function loginUser() {
     }
     else {
         $.get(address + ':3000/login-user', { email: email, password: password }, function (data) {
-            console.log(data);
-            if (data) {
+            if (data.email) {
                 $.get(address + ':3001/assign-session', { data: data }, function (data2) {
                     alert("Login Success");
-//                     window.location.replace(address + ":3001/");
+                    window.location.replace(address + ":3001/");
                 });
             }
             else {
