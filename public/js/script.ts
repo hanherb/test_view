8bbb28bb6e51f0353f6d144298f38c5903d20669
hanherb <dyo.pyon@gmail.com>
2018-12-30 15:50:07 +0700

    logout address change

--- public/js/script.ts
+++ public/js/script.ts
@@ -165,7 +165,7 @@ function loginUser() {
 	}
 	else {
 		$.get(address + ':3000/login-user', {email: email, password: password}, function(data) {
-			if(data) {
+			if(data.email) {
 				$.get(address + ':3001/assign-session', {data: data}, function(data2) {
 					alert("Login Success");
 					window.location.replace(address + ":3001/");
